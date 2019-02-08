78fc94786f9298b0ea21c506cb61f91b3fcf0c40
Danny <danny@Dannys-MacBook-Pro.local>
2019-02-08 18:22:33 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import Portfolio from './components/portfolio/portfolio';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
+import resumeData from './components/resume/resumeData';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header resumeData={resumeData} />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Contact />
+        <Footer />
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/components/resume/resumeData.js
@@ -0,0 +1,6 @@
+let resumeData = {
+  "imagebaseurl":"http://localhost:3000/",
+  "name": "Danny Schrader",
+  "role": "Fullstack Developer"
+  }
+export default resumeData
